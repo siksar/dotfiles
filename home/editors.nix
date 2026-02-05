8aad3d1aab20797c6407df6981fc84ecde459c19
zixar <halilbatuhanyilmaz@proton.me>
2026-02-05 17:33:05 +0300

    home

--- home/editors.nix
+++ home/editors.nix
@@ -23,7 +23,7 @@
       nui-nvim
       
       # Theme
-      gruvbox-nvim
+      tokyonight-nvim
       
       # UI
       lualine-nvim
@@ -101,26 +101,33 @@
       opt.guifont = "JetBrainsMono Nerd Font:h12"
       
       -- ====================================================================
-      -- GRUVBOX THEME
+      -- TOKYO NIGHT THEME
       -- ====================================================================
-      require("gruvbox").setup({
-        contrast = "hard",
-        transparent_mode = false,
-        italic = {
-          strings = false,
-          comments = true,
-          operators = false,
+      require("tokyonight").setup({
+        style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+        light_style = "day", -- The theme is used when the background is set to light
+        transparent = false, -- Enable this to disable setting the background color
+        terminal_colors = true, -- Configure the colors for the neovim terminal
+        styles = {
+          -- Style to be applied to different syntax groups
+          -- Value is any valid attr-list value for `:help nvim_set_hl`
+          comments = { italic = true },
+          keywords = { italic = true },
+          functions = {},
+          variables = {},
+          -- Background styles. Can be "dark", "transparent" or "normal"
+          sidebars = "dark", -- style for sidebars, see below
+          floats = "dark", -- style for floating windows
         },
       })
-      vim.cmd.colorscheme("gruvbox")
-      vim.opt.background = "dark"
+      vim.cmd.colorscheme("tokyonight")
       
       -- ====================================================================
       -- LUALINE
       -- ====================================================================
       require("lualine").setup({
         options = {
-          theme = "gruvbox",
+          theme = "tokyonight",
           component_separators = { left = "", right = "" },
           section_separators = { left = "", right = "" },
         },
